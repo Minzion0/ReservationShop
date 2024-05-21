3d08eb352c0b62deba91a7e84034906794cc8026
minzion <aert12435@gmail.com>
2024-05-21 14:40:57 +0900

    start project

---
+++ src/main/java/com/example/reservationshop/ReservationShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reservationshop/ReservationShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
